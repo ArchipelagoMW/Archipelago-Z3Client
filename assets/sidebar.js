16c770ec1caef958882fa9b74e2b4d16902c1cd9
Chris Wilson <chris@legendserver.info>
2021-03-30 16:51:54 -0400

    Minor style adjustments

--- assets/sidebar.js
+++ assets/sidebar.js
@@ -2,9 +2,12 @@ window.addEventListener('load', () => {
   // Allow the user to toggle the sidebar
   document.getElementById('sidebar-toggle-button').addEventListener('click', () => {
     const sidebar = document.getElementById('sidebar');
+    const collapseButton = document.getElementById('sidebar-toggle-button');
     if (sidebar.classList.contains('collapsed')) {
-      return sidebar.classList.remove('collapsed');
+      sidebar.classList.remove('collapsed');
+      return collapseButton.innerText = '↪';
     }
     sidebar.classList.add('collapsed');
+    collapseButton.innerText = '↩';
   });
 });
