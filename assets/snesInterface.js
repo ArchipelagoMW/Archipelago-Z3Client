d80523fff3a6f8807e18e637cad036f137bd0453
Chris Wilson <chris@legendserver.info>
2021-06-26 16:56:11 -0400

    Add FXPak CMD code to snesInterface, unused for the moment as FXPak CMD is not supported by SNI yet

--- assets/snesInterface.js
+++ assets/snesInterface.js
@@ -110,11 +110,46 @@ const readFromAddress = (hexOffset, byteCountInHex) => new Promise(async (resolv
 /**
  * Write data to a SNES device
  * @param hexOffset Location to begin reading from SNES memory
- * @param binaryData Data to be written to the ROM
+ * @param data A Uint8Array of data to be written to the ROM
  * @return Promise which resolves when the SNES has completed writing its new data
  */
-const writeToAddress = (hexOffset, binaryData) => new Promise((resolve, reject) => {
-  window.sni.writeToAddress(hexOffset, binaryData)
+const writeToAddress = (hexOffset, data) => new Promise((resolve, reject) => {
+  // TODO: The FXPak CMD feature is not currently supported by SNI. When it is, the below code is designed to use it
+  // The FXPak prevents direct writes to WRAM (a good idea, honestly) and has a feature called CMD
+  // which allows you to transfer things into WRAM in a more controlled manner. That is what we're using here.
+  /*
+  if (fxPakMode) {
+    // Data which will be written to FXPak
+    const bytes = [];
+
+    // Store the SNES cpu state
+    [0x00, 0xE2, 0x20, 0x48, 0xEB, 0x48].forEach((data) => bytes.push(data));
+
+    // Write data to SRAM and transfer to WRAM
+    for (let i = 0; i < data.length; i++) {
+      const memoryAddress = hexOffset + 0x7E0000 - WRAM_START + i;
+      bytes.push(0xA9); // LDA (write the following byte into the accumulator)
+      bytes.push(data[i]); // Byte to be written
+      bytes.push(0x8F); // STA.l (store accumulator to memory, absolute long indexed)
+      bytes.push(memoryAddress & 0xFF); // Data written to accumulator
+      bytes.push((memoryAddress >> 8) & 0xFF); // Data written to accumulator
+      bytes.push((memoryAddress >> 16) & 0xFF); // Data written to accumulator
+    }
+
+    // Restore the SNES cpu state
+    [0xA9, 0x00, 0x8F, 0x00, 0x2C, 0x00, 0x68, 0xEB, 0x68, 0x28, 0x6C, 0xEA, 0xFF, 0x08]
+      .forEach((data) => bytes.push(data));
+
+    const writeData = new Uint8Array(bytes.length);
+    writeData.set(bytes);
+    return window.sni.writeToAddress(hexOffset, writeData)
+      .then((result) => resolve(result))
+      .catch((err) => reject(err));
+  }
+   */
+
+  // Most emulators (snes9x, RetroArch, BizHawk, BSNES, etc.) will allow you to write directly to WRAM
+  window.sni.writeToAddress(hexOffset, data)
     .then((result) => resolve(result))
     .catch((err) => reject(err));
 });
